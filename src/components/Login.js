8925ad8e4f435b7ff1a069cbe06169e7d76d97b0
DheerajSingh199 <dheerajsingh16496@gmail.com>
2024-05-28 08:56:35 +0530

    login and signup page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,35 @@
-import React, { useState } from "react";
-import Header from "./Header";
+import React, { useState, useRef } from "react";
+import Header from "./Header.js";
+import { Validate } from "../utils/Validate.js";
 
 const Login = () => {
   const [IsSingInFrom, setIsSingInFrom] = useState(true);
+  const [ErrMessage, setErrMessage] = useState(null);
+
+  const email = useRef(null);
+  const Ispassword = useRef(null);
+
+  const handleButtonClick = () => {
+    const message = Validate(email.current.value, Ispassword.current.value);
+    setErrMessage(message);
+    console.log(message);
+  };
+
   const toggleSingInFrom = () => {
     setIsSingInFrom(!IsSingInFrom);
   };
+
   return (
     <div>
-      <Header/>
+      <Header />
       <div>
         <img
-          className="absolute "
+          className=" hidden sm:absolute sm:inline "
           src="https://assets.nflxext.com/ffe/siteui/vlv3/a99688ca-33c3-4099-9baa-07a2e2acb398/12b8ef67-2ff8-428c-8fc7-5388868ee28d/CH-en-20240520-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="backGroundImg"
         />
       </div>
-      <from className=" w-4/12 absolute bg-black my-36 mx-auto right-0 left-0 p-5 opacity-90 rounded-lg ">
+      <form onSubmit={(e)=> e.preventDefault()} className=" my-16 sm:my-36 sm:w-4/12 absolute bg-black  mx-auto right-0 left-0 p-5 opacity-90 rounded-lg ">
         <h1 className="font-bold text-3xl p-2 text-white">
           {IsSingInFrom ? "Sign In" : "Sign Up"}
         </h1>
@@ -28,18 +41,30 @@ const Login = () => {
           />
         )}
         <input
+          ref={email}
           className="p-4 mt-4 block w-5/6 ml-5 text-white bg-gray-900"
           type="text"
           placeholder="Email or mobile number  "
         />
+        
+
         <input
+          ref={Ispassword}
           className="p-4 mt-4 block w-5/6 ml-5 text-white bg-gray-900"
           type="password"
           placeholder="Password"
+          
         />
+        <p className="text-red-500 mt-1 text-lg text-center hover:underline">
+          {ErrMessage}
+        </p>
+        
 
-        <button className="h-12 rounded-lg   mt-4 bg-red-700  text-white w-5/6 ml-5 text-lg font-bold hover:bg-red-800">
-          {IsSingInFrom ? "Sign In" : "Sign Up"}
+        <button 
+          onClick={handleButtonClick}
+          className="h-12 rounded-lg   mt-4 bg-red-700  text-white w-5/6 ml-5 text-lg font-bold hover:bg-red-800"
+        >
+          {IsSingInFrom ? "Sign Ins" : "Sign Up"}
         </button>
         <p className="mt-4 text-white text-lg text-center">OR</p>
         <button className="h-12 rounded-lg text-lg  mt-4 bg-gray-600  text-white w-5/6 ml-5 font-bold hover:bg-gray-700">
@@ -54,22 +79,20 @@ const Login = () => {
           name="RememberMe"
           className="scale-150 ml-6 mt-5"
         />
-        <label className=" text-white text-lg pl-2 ml-2" for="RememberMe">
-          Remember me
-        </label>
+        <label className=" text-white text-lg pl-2 ml-2">Remember me</label>
         <br></br>
         <div className="flex justify-center items-center">
-        {!IsSingInFrom && (
-          <p className="mt-4 text-gray-400 text-lg text-center inline-block">
-            New to Netflix?{" "}
+          {!IsSingInFrom && (
+            <p className="mt-4 text-gray-400 text-lg text-center inline-block">
+              New to Netflix?{" "}
+            </p>
+          )}
+          <p
+            className=" mt-4 text-xl text-center text-white hover:underline cursor-pointer inline-block ml-2 "
+            onClick={toggleSingInFrom}
+          >
+            {IsSingInFrom ? "Sign In" : "Sign up now"}
           </p>
-        )}
-        <p
-          className=" mt-4 text-xl text-center text-white hover:underline cursor-pointer inline-block ml-2 "
-          onClick={toggleSingInFrom}
-        >
-          {IsSingInFrom ? "Sign In" : "Sign up now"}
-        </p>
         </div>
 
         <p className="mt-4 text-gray-400 text-sm">
@@ -78,16 +101,10 @@ const Login = () => {
             Learn more.
           </span>
         </p>
-      </from>
+      </form>
 
-      <div className="-z-20 bg-black w-full h-screen fixed ">
-        
+      <div className="-z-20 bg-black w-full h-screen fixed "></div>
     </div>
-
-   
-      
-    </div>
-    
   );
 };
 
