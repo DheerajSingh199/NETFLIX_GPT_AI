0dce16546ba02c661aab04ab3cb98f02dbb41661
DheerajSingh199 <dheerajsingh16496@gmail.com>
2024-09-28 00:42:57 +0530

    last push

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,3 +1,5 @@
+
+
 export const BACKGROUNDIMG = "https://assets.nflxext.com/ffe/siteui/vlv3/a99688ca-33c3-4099-9baa-07a2e2acb398/12b8ef67-2ff8-428c-8fc7-5388868ee28d/CH-en-20240520-popsignuptwoweeks-perspective_alpha_website_large.jpg"
 export const  LOGO= "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 export const MOVIE_OPTION = {
@@ -7,6 +9,6 @@ export const MOVIE_OPTION = {
       Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MDFjOWEwOWRkOTU4Yzg1MmMxMDMwMDc5N2JiODQwMSIsInN1YiI6IjY2NzQ4MWFjMDhmMmQ5MTRkY2M0ZjcyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2-_LJgL8gO2ZXcWR8DfxU_39zbPe87NvF7KW3OVntRQ",
     }
   };
-  export const OPENAI_KEY = "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"
+  export const OPENAI_KEY = process.env.REACT_APP_Open_Ai_Key
 
 export const IMG_CDN = "https://image.tmdb.org/t/p/w500"
